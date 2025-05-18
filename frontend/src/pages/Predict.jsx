ca2f9123c5bdc11af5e541230c8cec7a816ebee2
toshan kanwar <162002505+toshankanwar@users.noreply.github.com>
2025-05-19 01:58:38 +0530

    Update Predict.jsx

--- frontend/src/pages/Predict.jsx
+++ frontend/src/pages/Predict.jsx
@@ -42,15 +42,8 @@ const handleSubmit = async (e) => {
     console.log("Processing prediction request...");
     
     try {
-         // const response = await axios.post(`${process.env.REACT_APP_API_URL}/predict`, form);
-        const response = await axios.post(
-            'https://hfailure-backend-2.onrender.com/predict',
-            form,
-            {
-                headers: { 'Content-Type': 'application/json' }
-            }
-        );
-        
+       const response = await axios.post(`${process.env.REACT_APP_API_URL}/predict`, form);
+  
         
         if (response.data && response.data.prediction) {
             setResult(response.data);
