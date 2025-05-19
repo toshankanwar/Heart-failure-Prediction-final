9174130b04cc425f20695a9db97bff4a880e0720
toshan kanwar <162002505+toshankanwar@users.noreply.github.com>
2025-05-19 12:40:15 +0530

    Update Predict.jsx

--- frontend/src/pages/Predict.jsx
+++ frontend/src/pages/Predict.jsx
@@ -43,7 +43,7 @@ export default function Predict() {
     e.preventDefault();
     setIsLoading(true);
     try {
-      const res = await axios.post(`${process.env.REACT_APP_API_URL}/predict`, form);
+      const res = await axios.post("https://hfailure.onrender.com/predict", form);
       const predictionData = res.data;
       setResult(predictionData);
 
